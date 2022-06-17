6ec0f0bc1282327f02f16bd91c964135719f1b3b
George Bobby <georgebobbyawesome@gmail.com>
2022-06-17 11:07:18 +0530

    Add files via upload

---
+++ plugins/commands.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Dark Angel
+
+import os
+from config import Config
+from translation import Translation
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@Client.on_message(filters.private & filters.command(['start']))
+async def start(client, message):
+    buttons = [[
+        InlineKeyboardButton('📜 Support Group', url='https://t.me/DxHelpDesk'),
+        InlineKeyboardButton('Update Channel ♻️', url='https://t.me/DX_Botz')
+    ],[
+        InlineKeyboardButton('SouceCode 💡', url='https://github.com/Jijinr/Frwdit')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.START_TXT.format(
+                message.from_user.first_name),
+        parse_mode="html")
+
+@Client.on_message(filters.private & filters.command(['help']))
+async def help(client, message):
+    buttons = [[
+        InlineKeyboardButton('close 🔐', callback_data='close_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.HELP_TXT,
+        parse_mode="html")
+
+@Client.on_message(filters.private & filters.command(['about']))
+async def about(client, message):
+    buttons = [[
+        InlineKeyboardButton('💡 SouceCode', url='https://github.com/Jijinr/Frwdit'),
+        InlineKeyboardButton('close 🔐', callback_data='close_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(buttons)
+    await client.send_message(
+        chat_id=message.chat.id,
+        reply_markup=reply_markup,
+        text=Translation.ABOUT_TXT,
+        disable_web_page_preview=True,
+        parse_mode="html"
+    )
+
+        
